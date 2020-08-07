87bf3a3568c7f33b55ad77cf864583f9c2cc15ae
Zied <26070035+zguesmi@users.noreply.github.com>
2020-08-07 08:30:58 +0200

    Clean

--- build/jitsi.py
+++ build/jitsi.py
@@ -251,20 +251,3 @@ if __name__ == "__main__":
         log.error("Error in main thread")
         log.exception(e)
         exit(1)
-
-# class GracefulKiller:
-#     kill_now = False
-#     def __init__(self):
-#         signal.signal(signal.SIGINT, self.exit_gracefully)
-#         signal.signal(signal.SIGTERM, self.exit_gracefully)
-#         # signal.signal(signal.SIGKILL, self.exit_gracefully)
-
-#     def exit_gracefully(self, signum, frame):
-#         self.kill_now = True
-
-# if __name__ == "__main__":
-#     killer = GracefulKiller()
-#     while not killer.kill_now:
-#         time.sleep(1)
-#         print("doing something in a loop ...")
-#     print("End of the program. I was killed gracefully :)")
